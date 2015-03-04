b7ffc7eb1d785d4fc25a0f434fce4f3b0d7e7bbc
probe <re2rar@gmail.com>
2015-03-03 16:24:37 -0800

    Added moloco sdk

---
+++ app/src/main/java/com/moloco/ads/sdksample/MolocoApplication.java
@@ -0,0 +1,19 @@
+package com.moloco.ads.sdksample;
+
+import android.app.Application;
+
+import com.moloco.ads.context.MolocoEntryPoint;
+
+public class MolocoApplication extends Application {
+    
+    private MolocoEntryPoint molocoEntryPoint;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        molocoEntryPoint = MolocoEntryPoint.newInstance(this);
+        molocoEntryPoint.init();
+    }
+
+    
+}
